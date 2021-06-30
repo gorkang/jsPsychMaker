79fd7c327267df5457ee5fb25dfa1da8ba85c2d2
G <gorkang@gmail.com>
2021-06-30 18:29:22 +0100

    added two new tasks. ESM and EmpaTom

---
+++ canonical_protocol/tasks/ESM.js
@@ -0,0 +1,127 @@
+/**
+ * CSCN lab
+/**
+This document was made with test_maker
+*/
+
+onkeydown = function block_fkeys(event){
+    var x = event.which || event.keyCode;
+    if(x == 112 || x == 116){
+        console.log("Blocked key");
+        event.preventDefault();
+        return false;
+    }else{
+        return;
+    }
+}
+
+questions = ( typeof questions != 'undefined' && questions instanceof Array ) ? questions : [];;    //final timeline
+
+ESM = [];    //temporal timeline
+
+  // preloading videos only works when the file is running on a server
+  // if you run this experiment by opening the file directly in the browser,
+  // then video preloading will be disabled to prevent CORS errors
+
+  var instruction_screen_experiment = {
+    type: 'html-button-response',
+    stimulus: '<div style="max-width:600px;"><p>Descripcion de la tarea e instrucciones. ....</p></div>',
+    choices: ['Continue']
+  }
+
+  var question01 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Cómo está su memoria en comparación a como estaba 10 años atrás?", options: ['Mejor', 'Igual', 'Peor'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_01', procedure: 'ESM'}
+  };
+  ESM.push(question01);
+
+  var question02 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Siente que le cuesta más recordar las cosas de lo que le costaba hace un año atrás?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_02', procedure: 'ESM'}
+  };
+  ESM.push(question02);
+
+  var question03 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Cómo está su capacidad para recordar los nombres de personas cercanas o familiares?", options: ['Muy Buena', 'Buena', 'Mala', 'Muy Mala'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_03', procedure: 'ESM'}
+  };
+  ESM.push(question03);
+
+  var question04 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Siente usted que su memoria está peor que antes?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_04', procedure: 'ESM'}
+  };
+  ESM.push(question04);
+
+  var question05 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Siente que se está olvidando donde deja las cosas?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_05', procedure: 'ESM'}
+  };
+  ESM.push(question05);
+
+  var question06 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Tiene más dificultades que antes de encontrar las palabras necesarias para tener una conversación?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_06', procedure: 'ESM'}
+  };
+  ESM.push(question06);
+
+  var question07 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Cómo está su capacidad para recordar compromisos importantes?", options: ['Muy Buena', 'Buena', 'Mala', 'Muy Mala'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_07', procedure: 'ESM'}
+  };
+  ESM.push(question07);
+
+  var question08 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Tiene dificultades para recordar cosas que han pasado recientemente?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_08', procedure: 'ESM'}
+  };
+  ESM.push(question08);
+
+  var question09 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Tiende a olvidar las cosas que le dijeron ayer o hace pocos días atrás?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_09', procedure: 'ESM'}
+  };
+  ESM.push(question09);
+
+  var question10 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Le ha pasado que comienza a hacer algo pero se le olvida lo que estaba haciendo?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_10', procedure: 'ESM'}
+  };
+  ESM.push(question10);
+
+  var question11 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "¿Le ha pasado que entra a una pieza a buscar o hacer algo y se le olvida lo que era?", options: ['Si', 'No', 'No se'], required: true, horizontal: false}],
+    data: {trialid: 'ESM_11', procedure: 'ESM'}
+  };
+  ESM.push(question11);
+
+
+
+
+  ESM.unshift(instruction_screen_experiment);
+
+  questions.push.apply(questions, ESM)
+
+  questions.push({
+      type: 'call-function',
+      func: function(){
+        if (online) {
+          var data = jsPsych.data.get().filter({procedure: 'ESM'}).csv();
+        } else {
+          var data = jsPsych.data.get().filter({procedure: 'ESM'}).json();
+        }
+        saveData(data, online, 'ESM');
+      },
+      procedure: 'ESM'
+  });
---
+++ canonical_protocol/tasks/EmpaTom.js
@@ -0,0 +1,92 @@
+
+/**
+ * CSCN lab
+/**
+This document was made with test_maker
+*/
+
+onkeydown = function block_fkeys(event){
+    var x = event.which || event.keyCode;
+    if(x == 112 || x == 116){
+        console.log("Blocked key");
+        event.preventDefault();
+        return false;
+    }else{
+        return;
+    }
+}
+
+questions = ( typeof questions != 'undefined' && questions instanceof Array ) ? questions : [];;    //final timeline
+
+EmpaTom = [];    //temporal timeline
+
+  // preloading videos only works when the file is running on a server
+  // if you run this experiment by opening the file directly in the browser,
+  // then video preloading will be disabled to prevent CORS errors
+  var preload = {
+    type: 'preload',
+    auto_preload: true
+  }
+
+  var instruction_screen_experiment = {
+    type: 'html-button-response',
+    stimulus: '<div style="max-width:600px;"><p>Descripcion de la tarea e instrucciones. Tom tiene Alzheimer....</p></div>',
+    choices: ['Continue']
+  }
+
+  var question01 = {
+    type: 'video-button-response',
+    stimulus: ['vid/Entrevista1.mp4'],
+    choices: ['He visto el video'],
+    margin_vertical: '10px',
+    margin_horizontal: '8px',
+    prompt: '',
+    width: 600,
+    autoplay: true,
+    rate: 1,
+    response_ends_trial: true,
+    response_allowed_while_playing: false,
+    data: {trialid: 'EmpaTom_01', procedure: 'EmpaTom'}
+  }
+  EmpaTom.push(question01);
+
+  var question02 = {
+    type: 'html-slider-response',
+    stimulus: "<div class='justified'>¿Cómo te sientes?</div></br>", require_movement:true, required: true, min: 1, max: 7, slider_width: 500, start: 4, step: 1, labels: ["Mal", "Neutro", "Bien"], button_label: "Siguiente",
+    data: {trialid: 'EmpaTom_02', procedure: 'EmpaTom'}
+  };
+  EmpaTom.push(question02);
+
+  var question03 = {
+    type: 'html-slider-response',
+    stimulus: "<div class='justified'>¿Cuánta compasión sentiste por Tom?</div></br>", require_movement:true, required: true, min: 1, max: 7, slider_width: 500, start: 4, step: 1, labels: ["Nada", "Mucha"], button_label: "Siguiente",
+    data: {trialid: 'EmpaTom_03', procedure: 'EmpaTom'}
+  };
+  EmpaTom.push(question03);
+
+  var question04 = {
+    type: 'survey-multi-choice-vertical',
+    questions: [{prompt: "<div class='justified'>Tom piensa.</div>", options: ['&nbsp;que es muy probable que olvide a su esposa producto del alzheimer', '&nbsp;que lo que menos desea es olvidarse de su esposa producto del alzheimer', '&nbsp;que se dará por vencido y olvidará a su esposa producto del alzheimer'], required: true, horizontal: false}],
+
+    data: {trialid: 'EmpaTom_04', procedure: 'EmpaTom'}
+  };
+  EmpaTom.push(question04);
+
+
+  EmpaTom.unshift(instruction_screen_experiment);
+  EmpaTom.unshift(preload);
+
+  questions.push.apply(questions, EmpaTom)
+
+  questions.push({
+      type: 'call-function',
+      func: function(){
+        if (online) {
+          var data = jsPsych.data.get().filter({procedure: 'EmpaTom'}).csv();
+        } else {
+          var data = jsPsych.data.get().filter({procedure: 'EmpaTom'}).json();
+        }
+        saveData(data, online, 'EmpaTom');
+      },
+      procedure: 'EmpaTom'
+  });
