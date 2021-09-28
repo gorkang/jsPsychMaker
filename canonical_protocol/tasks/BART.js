07c2f2df852912ec467bfabd703412067d0e09f3
G <gorkang@gmail.com>
2021-09-28 15:00:58 +0100

    updates to canonical protocol

---
+++ canonical_protocol/tasks/BART.js
@@ -0,0 +1,224 @@
+/**
+ * CSCN lab
+/**
+This document was made with test_maker
+*/
+
+onkeydown = function block_fkeys(event){
+  var x = event.which || event.keyCode;
+  if(x == 112 || x == 116){
+    console.log("Blocked key");
+    event.preventDefault();
+    return false;
+  } else {
+    return;
+  }
+}
+
+function pad(num, size) {
+  num = num.toString();
+  while (num.length < size) num = "0" + num;
+  return num;
+}
+
+max_height = 609 * 0.8559;
+max_width = 555 * 0.8559;
+
+var repetition_count = 0;
+
+//base = 555x609 -> 540x960
+//max = 128;
+
+height = max_height*0.2159;
+width = max_width*0.2159;
+
+// 0.8559 max 119x131
+// 0.2159 min
+// + 0.50
+
+actual_money = 0;
+actual_rounds = 0;
+total_money = 0;
+
+// numbers for probabilities
+numbers = [1, 5, 10, 14, 18, 23, 27, 31, 36, 40, 44, 49, 53, 57, 62, 66, 70, 75, 79, 83, 88, 92, 96, 101, 105, 109, 114, 118, 122, 128];
+numbers = jsPsych.randomization.repeat(numbers, 1)
+
+balloon_img = images[0];
+audio_inflate = audios[0];
+audio_explode = audios[1];
+audio_collect = audios[2];
+audio_silence = audios[3];
+
+actual_status = "safe";
+
+// Retorna un entero aleatorio entre min (incluido) y max (incluido)
+// ¡Usando Math.round() dará una distribución no-uniforme!
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
+
+function inflate(actual_number) {
+  balloon = document.getElementById("BART_redBalloon");
+  actual_rounds = parseInt(balloon.getAttribute("status")) + 1;
+  balloon.setAttribute("status", actual_rounds);
+  balloon.width = width + max_width * (0.005 * actual_rounds);
+  balloon.height = height + max_height * (0.005 * actual_rounds);
+  actual_money = actual_money + 50;
+  document.getElementById("prompt").innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;Actualmente acumulado: ' + actual_money + ' <br> &nbsp;&nbsp;&nbsp;&nbsp;Número de infladas: ' + actual_rounds + ' <br> &nbsp;&nbsp;&nbsp;&nbsp;Total Acumulado: ' + total_money;
+
+  //actual_number = numbers[Math.floor(Math.random()*numbers.length)];
+
+  console.log(actual_number)
+
+  explode = (actual_rounds >= actual_number);
+
+  actual_audio = explode ? audio_explode : audio_inflate;
+
+  // audio:
+  var context = jsPsych.pluginAPI.audioContext();
+  var audio;
+
+  jsPsych.pluginAPI.getAudioBuffer(actual_audio)
+    .then(function (buffer) {
+      if (context !== null) {
+        audio = context.createBufferSource();
+        audio.buffer = buffer;
+        audio.connect(context.destination);
+      } else {
+        audio = buffer;
+        audio.pause();
+        audio.currentTime = 0;
+      }
+      setupTrial();
+    })
+    .catch(function (err) {
+      console.error(err)
+    });
+
+  function setupTrial() {
+    if (context !== null) {
+      startTime = context.currentTime;
+      audio.start(startTime);
+    } else {
+      audio.play();
+    }
+  }
+
+  document.getElementById("inflate_button").setAttribute("disabled", true);
+  document.getElementById("collect_button").setAttribute("disabled", true);
+  if (explode) {
+    actual_money = 0;
+    balloon.width = 0;
+    balloon.height = 0;
+    actual_status = "explode";
+    setTimeout(() => {  jsPsych.finishTrial(); }, 500);
+  } else {
+    setTimeout(() => {  document.getElementById("inflate_button").removeAttribute("disabled"); document.getElementById("collect_button").removeAttribute("disabled"); }, 500);
+  }
+}
+
+function collect() {
+  // audio:
+  var context = jsPsych.pluginAPI.audioContext();
+  var audio;
+
+  jsPsych.pluginAPI.getAudioBuffer(audio_collect)
+    .then(function (buffer) {
+      if (context !== null) {
+        audio = context.createBufferSource();
+        audio.buffer = buffer;
+        audio.connect(context.destination);
+      } else {
+        audio = buffer;
+        audio.currentTime = 0;
+      }
+      setupTrial();
+    })
+    .catch(function (err) {
+      console.error(err)
+    });
+
+  function setupTrial() {
+    if (context !== null) {
+      startTime = context.currentTime;
+      audio.start(startTime);
+    } else {
+      audio.play();
+    }
+  }
+
+  document.getElementById("inflate_button").setAttribute("disabled", true);
+  document.getElementById("collect_button").setAttribute("disabled", true);
+  balloon = document.getElementById("BART_redBalloon");
+  balloon.width = 0;
+  balloon.height = 0;
+  setTimeout(() => {  jsPsych.finishTrial(); }, 500);
+}
+
+questions = ( typeof questions != 'undefined' && questions instanceof Array ) ? questions : [];
+
+questions.push(
+  {
+    timeline: [{
+      type: 'fullscreen',
+      message: '<p>El experimento entrará en modo pantalla completa</p>',
+      button_label: 'Pantalla Completa',
+      delay_after: 0,
+      fullscreen_mode: true,
+      data: {procedure: 'BART'}
+    }],
+    conditional_function: function(){
+      if(window.innerWidth != screen.width || window.innerHeight != screen.height)
+        return true;
+      else
+        return false;
+    },
+    procedure: 'BART'
+  }
+);
+
+BART = [];
+
+for (var i = 0; i < 30; i++) {
+  explode_rounds = numbers.pop();
+  var question = {
+    type: 'audio-button-response',
+    stimulus: audio_silence,
+    choices: balloon_img,
+    block_size: [max_height, max_width],
+    size: [width, height],
+    button_html: ['<button class="jspsych-btn" id="inflate_button" onclick="inflate(' + explode_rounds + ')">INFLAR GLOBO</button>', '<button onclick="collect()" id="collect_button" class="jspsych-btn">$ RECAUDAR $</button>'],
+    prompt: function () {
+      return '<p id="prompt">&nbsp;&nbsp;&nbsp;&nbsp;Actualmente acumulado: ' + actual_money + ' <br> &nbsp;&nbsp;&nbsp;&nbsp;Número de infladas: ' + actual_rounds + ' <br> &nbsp;&nbsp;&nbsp;&nbsp;Total Acumulado: ' + total_money + '</p>';
+    },
+    data: {trialid: 'BART_' + pad(i + 1, 2), procedure: 'BART', stimulus: {explodeRounds: explode_rounds}, response: {status: "safe", rounds: actual_rounds, roundMoney: actual_money, totalMoney: total_money} },
+    on_finish: function (data) {
+      total_money += actual_money;
+      data.response.roundMoney = actual_money;
+      data.response.rounds = actual_rounds;
+      data.response.totalMoney = total_money;
+      data.response.status = actual_status;
+      actual_rounds = 0;
+      actual_money = 0;
+      actual_status = "safe";
+    }
+  }
+
+  BART.push(question);
+}
+
+questions.push.apply(questions, BART);
+
+questions.push({
+    type: 'call-function',
+    func: function(){
+      if (online) {
+        var data = jsPsych.data.get().filter({procedure: 'BART'}).csv();
+      } else {
+        var data = jsPsych.data.get().filter({procedure: 'BART'}).json();
+      }
+      saveData(data, online, 'BART');
+    },
+    procedure: 'BART'
+});
