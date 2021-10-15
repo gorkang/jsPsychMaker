e7ea7a32ee6955077e96cd1fb103a18b40a16581
G <gorkang@gmail.com>
2021-10-15 18:46:01 +0100

    Add new JS based Consent. No more CORS for local protocols.

---
+++ canonical_protocol_DEV/config_CANONICAL.js
@@ -0,0 +1,59 @@
+// Configuration file
+
+// Main parameters -------------------------------------------------------------
+pid = 999; // Protocol ID [number]
+online = true; // Protocol runs online [true/false]
+max_participants = 30; // Max participants per contition [number]
+random_id = false; // Assign random id to participants [true/false]
+max_time = "24:00:00"; // Max time to complete the protocol [HH:MM:SS]
+accept_discarded = true; // If an user is discarded (out of time), should be allow her to continue? (given there are free slots) [true/false]
+debug_mode = true; // SHOULD be false in production
+
+
+// INTRO [index.html] ----------------------------------------------------------
+intro_HTML = '<div title="logo"><p style="margin-bottom: 0.2in; line-height: 100%"><img src="controllers/media/logo.png" name="UAI" align="bottom" width="200" height="44" border="0"/></p></div>' +
+  'Para cambiar este texto, edita la variable intro_HTML en config.js<BR><BR>' +
+  'Si tienes alguna duda, puedes escribirnos a: <mailto::CORREO@CORREO.COM>CORREO@CORREO.COM</mailto><BR><BR>' +
+  'Te recomendamos usar <a href = "https://www.google.com/chrome/">Google Chrome <img src="controllers/media/compatible_chrome.gif" name="Chrome" align="bottom" border="0"/></a>';
+
+
+// ORDER OF TASKS --------------------------------------------------------------
+
+// CANONICAL_PROTOCOL TESTING ---
+  // Contains all the available tasks
+  
+first_tasks = ['Consent']; // First tasks (in sequential order)
+last_tasks = ['Goodbye']; // Last tasks (in sequential order)
+
+// All the tasks where the order of items is sequential in debug_mode 
+secuentially_ordered_tasks_1 = ["AIM", "BNT", "bRCOPE", "CAS", "Cov19Q", "COVIDCONTROL", "CRS", "CRT7", "CRTMCQ4", "CRTv", "DASS21", "DEBRIEF", "DEMOGR", "EAR", "ERQ", "ESM", "FDMQ", "GHQ12", "IDQ", "IEC", "IRI", "IRS", "MIS", "OBJNUM", "OTRASRELIG", "PBS", "PSETPP", "PSPPC", "PSS", "PVC", "PWb", "REI40", "Report", "RSS", "RTS", "SASS", "SBS", "SCSORF", "SDG", "SRA", "SRBQP", "SRSav", "SWBQ", "WEBEXEC"];
+
+// Between tasks [random order unavoidable]
+secuentially_ordered_tasks_2 = ["HRPVB", "HRPVBpost", "PRFBM", "PRFBMpost", "FONDECYT", "INFCONS"];
+
+// More randomness
+secuentially_ordered_tasks_3 = ["EmpaTom", "BART"];
+
+// FINAL ARRAY
+tasks = ['first_tasks', 'secuentially_ordered_tasks_1', 'secuentially_ordered_tasks_2', 'secuentially_ordered_tasks_3','last_tasks'];
+
+
+
+
+// MEDIA preloading ------------------------------------------------------------
+message_str = 'El protocolo está cargando, espere un momento...'; // Message when preloading media
+
+// media to preload in protocol_controller
+img_folder = 'media/img/';
+audio_folder = 'media/audio/';
+
+images = [img_folder + 'VPP_low.png',  img_folder + 'VPP_high.png', img_folder + 'VPN_low.png',  img_folder + 'VPN_high.png', img_folder + 'BART_redBalloon.png'];
+audios = [audio_folder + 'BART_inflate.mp3', audio_folder + 'BART_explode.mp3', audio_folder + 'BART_collect.mp3', audio_folder + 'silence.mp3'];
+video = ['media/vid/Entrevista1.mp4'];
+
+
+
+// BETWEEN participants variables ----------------------------------------------
+all_conditions = {
+  "FONDECYT": {"type": ["Text", "Image"]}
+  };
--- tasks_DEV/tasks/ConsentHTML.js
+++ tasks_DEV/tasks/ConsentHTML.js
@@ -5,14 +5,17 @@ This document was made with test_maker
 */
 
 
+
 questions = ( typeof questions != 'undefined' && questions instanceof Array ) ? questions : [];
 questions.push( check_fullscreen('Consent') );
+
 Consent = [];    //temporal timeline
 
 
 var instruction_screen_experiment = {
     type: 'instructions',
-    pages: ['<p><left><b><big>Consentimiento informado</big></b><br /></p>'],
+    pages: ['<p><left><b><big>Consentimiento informado</big></b><br />'+
+    '' +'</p>'],
     data: {trialid: 'Instructions_001', procedure: 'Consent'},
     show_clickable_nav: true,
     on_trial_start: function(){
@@ -21,27 +24,20 @@ var instruction_screen_experiment = {
 };
 
 
-// Reads consent from media/consent/consent-placeholder.js
-var question01 = {
-  type: 'html-button-response',
-  stimulus: intro_CONSENT,
-  choices: ['acepto participar', 'rechazo participar'],
-  prompt: "<BR><BR>",
-  // If 'rechazo participar' is pressed, end experiment
-  on_finish: function(data){
-    if(jsPsych.data.get().values().find(x => x.trialid === 'Consent_001').button_pressed == 1){
-      jsPsych.endExperiment('Gracias por tu tiempo. Puedes cerrar esta pestaña.');
-    }
-  },
-    data: {
-    trialid: 'Consent_001',
-    procedure: 'Consent'
-   }
-  
-  
-};
-Consent.push(question01);
 
+// declare the block.
+var trial = {
+  type:'external-html',
+  url: "media/html/consent-placeholder.html",
+  cont_btn: "start",
+  data: {
+    trialid: 'Consent_001',
+    procedure: 'Consent',
+     stimulus: 'html/consent-nieves.html',
+     response: document.getElementById("start").id
+   }
+};
+Consent.push(trial);
 
 Consent.unshift(instruction_screen_experiment);
 Consent.push.apply(questions, Consent);
