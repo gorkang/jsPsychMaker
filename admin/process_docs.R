13a5ac10a67ea750046358da8e5c3cda3b3b4606
G <gorkang@gmail.com>
2022-10-04 10:59:13 +0100

    improve process_docs so only processes new files

--- admin/process_docs.R
+++ admin/process_docs.R
@@ -1,21 +1,46 @@
-process_docs <- function(FOLDER = "/home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS/2021 - FONDECYT Regular/AYUDANTE/SHARED-FONDECYT-Faridy/jsPsychR/docs/") {
+#' process_docs
+#' Make a copy of the docs after downgrading the resolution of the pdfs to "screen"
+#'
+#' @param FOLDER 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+process_docs <- function(FOLDER = "~/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/jsPsychR/SHARED-docs/docs") {
 
   library(dplyr)
   library(purrr)
   
-  INITIAL = list.files(FOLDER, recursive = TRUE, pattern = "pdf", full.names = TRUE)
-  INITIAL_INFO = file.info(INITIAL) |> tibble::as_tibble()
-  INITIAL_INFO |> summarize(SIZE = sum(size)/1024)
+  INITIAL = tibble::tibble(path_to_file = list.files(FOLDER, recursive = TRUE, full.names = TRUE),
+                           file_short = paste0("docs/", basename(dirname(path_to_file)), "/", basename(path_to_file)))
+
+  OUTPUT_folder_files = list.files("docs", recursive = TRUE, full.names = TRUE)  
+  FINAL_FILES_input = INITIAL$path_to_file[!INITIAL$file_short %in% OUTPUT_folder_files]
   
-  1:length(INITIAL) |> 
+  
+  1:length(FINAL_FILES_input) |> 
     purrr::walk(~{
-      IN_file = INITIAL[.x]
-      OUT_file = paste0("docs/", basename(dirname(INITIAL[.x])), "/", basename(IN_file))
-      cli::cli_alert_info("{IN_file} --> {OUT_file}")
-      # /ebook
-      COMMAND = paste0("gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sOutputFile='", OUT_file, "' '", IN_file, "'")
-      system(COMMAND)
-      # processx::run(command = "gs", args = c("-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4", "-dPDFSETTINGS=/ebook", paste0("-sOutputFile=", OUT_file), paste0(" ", IN_file)))
+      # .x = 2
+      IN_file = FINAL_FILES_input[.x]
+      OUT_file = gsub(".*(docs\\/.*)$", "\\1", IN_file)
+      
+      # Check if folder exists
+      DIR_out = gsub("^\\/(.*)$", "\\1", dirname(OUT_file))
+      if (!dir.exists(DIR_out)) dir.create(DIR_out)
+      
+      cli::cli_alert_info("{gsub('/home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/', '', IN_file)} --> {OUT_file}")
+      
+      if (tools::file_ext(FINAL_FILES_input[.x]) %in% c("pdf", "PDF")) {
+        # /ebook
+        COMMAND = paste0("gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sOutputFile='", OUT_file, "' '", IN_file, "'")
+        # processx::run(command = "gs", args = c("-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4", "-dPDFSETTINGS=/ebook", paste0("-sOutputFile=", OUT_file), paste0(" ", IN_file)))
+        GS_output = system(COMMAND, intern = TRUE)
+        
+      } else {
+        file.copy(from = IN_file, to = OUT_file, overwrite = TRUE)
+      }
+      
     })
 
 }
