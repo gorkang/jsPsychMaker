1a85eec995f2b6b61c0b0d8f40a5ca7b50706f11
G <gorkang@gmail.com>
2023-03-04 11:40:15 +0000

    clean up

--- protocols_DEV/22/config.js
+++ protocols_DEV/22/config.js
@@ -1,12 +1,12 @@
 // Configuration file
 
 // Main parameters -------------------------------------------------------------
-pid = 22; // Protocol ID [number]
-online = true; // Protocol runs online [true/false]
-max_participants = 100000000; // Max participants per contition [number]
-random_id = true; // Assign random id to participants [true/false]
-max_time = "24:00:00"; // Max time to complete the protocol [HH:MM:SS]
-accept_discarded = true; // If an user is discarded (out of time), should be allow her to continue? (given there are free slots) [true/false]
+pid = 22;
+online = true;
+max_participants = 100000000;
+random_id = true;
+max_time = "24:00:00";
+accept_discarded = false;
 debug_mode = true; // SHOULD be false in production
 
 var_researcher_email = `gorkang@edu.uai.cl`;
@@ -14,26 +14,20 @@ var_researcher_email = `gorkang@edu.uai.cl`;
 // INTRO [index.html] ----------------------------------------------------------
 intro_HTML = '<div title="logo"><p style="margin-bottom: 0.2in; line-height: 100%"><img src="media/html/logo-UAI.png" name="UAI" align="bottom" width="200" height="60" border="0"/></p></div>' +
   'Bienvenida/o,<BR><BR>' +
-  'La duración de este protocolo es de unos 5 minutos. Incluye unas pocas preguntas demográficas y 4 casos hipotéticos donde te pedimos que realices recomendaciones.<BR><BR>' +
+  'La duración de este protocolo es de entre 5 y 10 minutos. Incluye unas pocas preguntas demográficas y 4 casos hipotéticos donde te pedimos que realices recomendaciones.<BR><BR>' +
   'Si tienes alguna duda, puedes escribirnos a: <mailto::gorkang@edu.uai.cl>gorkang@edu.uai.cl</mailto><BR><BR>' +
   'Te recomendamos usar <a href = "https://www.google.com/chrome/">Google Chrome <img src="controllers/media/compatible_chrome.png" name="Chrome" align="bottom" border="0", height="24", width="24"/></a>';
 
 // ORDER OF TASKS --------------------------------------------------------------
 
-first_tasks = ['Consent']; // First tasks (in sequential order)
-last_tasks = ['Goodbye']; // Last tasks (in sequential order)
-
-// Create as many as needed.  The order of the tasks in the arrays starting with "random" will be randomized
-//randomly_ordered_tasks_1 = ['']; // Block of tasks in random order
-secuentially_ordered_tasks_1 = ['DEMOGRfondecyt2022E1', 'FONDECYT2022E1', 'BNT']; // Block of tasks in sequential order
-
-// FINAL ARRAY of tasks [build combining the above blocks]
-  // tasks SHOULD contain an array of strings. GOOD: tasks = ['my_tasks']; BAD: tasks = [my_tasks];
+first_tasks = ['Consent'];
+last_tasks = ['Goodbye'];
+secuentially_ordered_tasks_1 = ['DEMOGRfondecyt2022E1', 'FONDECYT2022E1', 'BNT'];
 tasks = ['first_tasks', 'secuentially_ordered_tasks_1', 'last_tasks'];
 
 // MEDIA preloading ------------------------------------------------------------
 
-message_str = 'El protocolo está cargando, espere un momento...'; // Message when preloading media
+message_str = 'El protocolo está cargando, espere un momento...';
 
 // media to preload in protocol_controller
 img_folder = 'media/img/';
@@ -48,9 +42,6 @@ zoom_type = 'fullPage';
 
 // BETWEEN subject variables ----------------------------------------------
 
-// If there is no between-subject task:
-//all_conditions = {"protocol": {"type": ["survey"]}};
-
-// If there are between-subject tasks:
+//TYPE IS A VERY BAD NAME (used a lot in javascript jsPsych)
 all_conditions = {"FONDECYT2022E1": {"type": ["Text", "Image"]}};
 
