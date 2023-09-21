da719e23847b52ddaf6670569af429c60668e60c
G <gorkang@gmail.com>
2023-09-21 13:46:52 +0100

    Add blacklist to avoid downsizing some files, and add a check for tasks with missing docs

--- admin/process_docs.R
+++ admin/process_docs.R
@@ -12,16 +12,25 @@ process_docs <- function(FOLDER = "~/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/j
   library(dplyr)
   library(purrr)
   
+  
+  # Some files can´t be optimized (e.g. Fig. 1 in PRFBM_en-US.pdf becomes unreadable)
+  BLACKLIST = c("PRFBM_en-US.pdf")
+  
+  # Initial files in jsPsychR/SHARED-docs/docs"
   INITIAL = tibble::tibble(path_to_file = list.files(FOLDER, recursive = TRUE, full.names = TRUE),
-                           file_short = paste0("docs/", basename(dirname(path_to_file)), "/", basename(path_to_file)))
-
+                           file_short = paste0("docs/", basename(dirname(path_to_file)), "/", basename(path_to_file))) 
+  
+  # Output in docs/
   OUTPUT_folder_files = list.files("docs", recursive = TRUE, full.names = TRUE)  
+  
+  # Only process the new files
   FINAL_FILES_input = INITIAL$path_to_file[!INITIAL$file_short %in% OUTPUT_folder_files]
   
-  
+  # Loop
   1:length(FINAL_FILES_input) |> 
     purrr::walk(~{
       # .x = 2
+      
       IN_file = FINAL_FILES_input[.x]
       OUT_file = gsub(".*(docs\\/.*)$", "\\1", IN_file)
       
@@ -31,10 +40,10 @@ process_docs <- function(FOLDER = "~/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/j
       
       cli::cli_alert_info("{gsub('/home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/', '', IN_file)} --> {OUT_file}")
       
-      if (tools::file_ext(FINAL_FILES_input[.x]) %in% c("pdf", "PDF")) {
-        # /ebook
+      # If it's a PDF and it is NOT in the BLACKLIST 
+      if (tools::file_ext(FINAL_FILES_input[.x]) %in% c("pdf", "PDF") & !basename(FINAL_FILES_input[.x]) %in% BLACKLIST) {
+        # For higher quality: /ebook
         COMMAND = paste0("gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sOutputFile='", OUT_file, "' '", IN_file, "'")
-        # processx::run(command = "gs", args = c("-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite", "-dCompatibilityLevel=1.4", "-dPDFSETTINGS=/ebook", paste0("-sOutputFile=", OUT_file), paste0(" ", IN_file)))
         GS_output = system(COMMAND, intern = TRUE)
         
       } else {
@@ -43,4 +52,23 @@ process_docs <- function(FOLDER = "~/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/j
       
     })
 
+
+  # CHECK missing docs ------------------------------------------------------
+
+  tasks_with_docs = unique(gsub("(.?)_.*", "\\1", tools::file_path_sans_ext(basename(INITIAL$file_short))))
+  
+  all_prepare_scripts = gsub("pre|post", "", gsub("^prepare_(.*)\\.R", "\\1", list.files("../jsPsychHelpeR/R_tasks/", pattern = "R$")))
+     
+  
+  MISSING_raw = unique(all_prepare_scripts[!all_prepare_scripts %in% tasks_with_docs])
+  
+  BLACKLIST = "Bank|Consent|ConsentHTML|DEBRIEF|Goodbye|Report|SDG|TEMPLATE|AIM[0-9]{1,3}|DEMOGR[0-9]{0,3}|FORM[0-9]{1,3}|FONDECYT.*"
+  
+  MISSING = MISSING_raw[!grepl(BLACKLIST, MISSING_raw)]
+  
+  cli::cli_alert_info("{length(MISSING)} tasks missing docs: ")
+  
+  return(MISSING)
+  
 }
+
