f0d0c6f518daec6dba391ed60adf1c2e7bca70d1
G <gorkang@gmail.com>
2023-09-25 09:42:15 +0100

    delete old files

--- protocols_DEV/31/media/consent/consent-placeholder.js
+++ protocols_DEV/31/media/consent/consent-placeholder.js
@@ -1,23 +1,23 @@
 // You can include regular HTML code between the ``, edit the template, and or fill the template variables
 
 // Template variables
-path_logo = "controllers/media/logo.png";
+path_logo = "media/html/logo-UAI.png";
 var_title = `Comprensión y toma de decisiones en población general y personal sanitario de pruebas de screening y diagnósticas: hacia la toma de decisiones conjunta`;
 var_researcher_name = `Gorka Navarrete`;
 var_researcher_email = `gorkang@gmail.com`;
 var_researcher_affiliation = `Centro de Neurociencia Social y Cognitiva`;
 var_researcher_university = `Universidad Adolfo Ibáñez`;
 
-var_ethics_comitee_name_president = `[[Nombre presidenta/e comité ética]]`;
-var_ethics_comitee_phone = `(562) 2331-1319`;
-var_ethics_comitee_email = `[[Email presidenta/e comité ética]]`;
+var_ethics_comitee_name_president = `Paola Cañon`;
+var_ethics_comitee_phone = `(562) 2331-1277`;
+var_ethics_comitee_email = `paola.canon.g@uai.cl`;
 
 
 var_purpose = `Con su firma, usted accede a participar en este proyecto. En él, estamos investigando como las personas razonan y toman decisiones en contextos médicos. Este estudio es financiado por FONDECYT`;
 var_procedure = `En este experimento, usted deberá completar algunas pruebas y cuestionarios en un computador. Su tarea consistirá en responder lo más fiel y cuidadosamente que pueda. El tiempo que tomará su participación variará dependiendo de lo rápido que complete las tareas. Sin embargo, estimamos que ese tiempo no superará los 10 minutos`;
 var_comprehension = `cómo las personas razonan y toman decisiones. También podrían beneficiar a las ciencias aplicadas, que usan hallazgos empíricos obtenidos de experimentos como éste`;
 
-
+/*
 // The content of intro_CONSENT will be shown in the Consent.js task
 intro_CONSENT = `
 
@@ -36,4 +36,31 @@ intro_CONSENT = `
 <BR><BR>
 </div>
 
+`;
+
+*/
+intro_CONSENT = `
+<div title="logo"><p style="margin-bottom: 0.2in; line-height: 100%"><img src="` + path_logo + `" name="UAI" align="bottom" width="200" height="44" border="0"/></p></div>
+<div>
+<h3 class="western" align="center">Informed Consent</h3><br/>
+Dear participant, <br/><br/>
+
+Thanks for your interest in our study! <br/><br/>
+
+In order to participate, you must be at least 50 years old, be a fluent English speaker and must reside in the US. <br/><br/>
+
+During your participation in this study, you will be presented with financial information, and will have to answer questions about hypothetical financial decisions. Other than that, you will encounter no greater risks or discomfort other than those incurred in routine daily activities. <br/><br/>
+
+Questions or concerns about this study, the use of your data, or interest in the final results of the study, may be directed to Prof. Nieves Valdés (nieves.valdes@uai.cl). <br/><br/>
+
+As an informed participant, by advancing to the next page I acknowledge: <br/><br/>
+
+My participation is voluntary and I may cease to take part in this study at any time, without penalty.<br/>
+All my questions about the experiment have been satisfactorily answered.<br/>
+The data collected will only be used to promote scientific knowledge and the researcher will use the data I provide for no purpose other than research.<br/>
+The data I provide is confidential.<br/><br/>
+
+<em>By selecting “I agree to participate” you declare that you are willing to voluntarily participate in this research: </em>
+<BR><BR>
+</div>
 `;
