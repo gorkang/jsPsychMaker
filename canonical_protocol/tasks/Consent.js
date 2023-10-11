146f6268999c2ab1be36bdb5af089ade76be0502
G <gorkang@gmail.com>
2023-10-11 10:19:06 +0100

    multilingual consent

--- canonical_protocol/tasks/Consent.js
+++ canonical_protocol/tasks/Consent.js
@@ -1,5 +1,30 @@
 /* CSCN - Created with jsPsychMaker: https://github.com/gorkang/jsPsychMaker */
+// Translations --------------------------------------------------------------
+switch (language) {
 
+  case "Spanish":
+
+    Consent_000 = ['<p><left><b><big>Consentimiento informado</big></b><br /></p>'];
+
+    Consent_001_choices = ['acepto participar', 'rechazo participar'];
+    Consent_001_end = 'Gracias por tu tiempo. Puedes cerrar esta página.';
+
+ 
+    break;
+
+
+  case "English":
+
+    Consent_000 = ['<p><left><b><big>Informed consent</big></b><br /></p>'];
+    Consent_001_choices = ['I agree to participate', 'I reject to participate'];
+    Consent_001_end = 'Thanks for your time. You can close this page.';
+    break;
+
+}
+
+
+
+// Task -----------------------------------------------------------------------
 questions = ( typeof questions != 'undefined' && questions instanceof Array ) ? questions : [];
 questions.push( check_fullscreen('Consent') );
 Consent = [];    //temporal timeline
@@ -7,7 +32,7 @@ Consent = [];    //temporal timeline
 
 var instruction_screen_experiment = {
     type: 'instructions',
-    pages: ['<p><left><b><big>Consentimiento informado</big></b><br /></p>'],
+    pages: Consent_000,
     data: {trialid: 'Consent_000', procedure: 'Consent'},
     show_clickable_nav: true,
     on_trial_start: function(){
@@ -20,20 +45,20 @@ var instruction_screen_experiment = {
 var question01 = {
   type: 'html-button-response',
   stimulus: intro_CONSENT,
-  choices: ['acepto participar', 'rechazo participar'],
+  choices: Consent_001_choices,
   prompt: "<BR><BR>",
   // If 'rechazo participar' is pressed, end experiment
   on_finish: function(data){
     if(jsPsych.data.get().values().find(x => x.trialid === 'Consent_001').button_pressed == 1){
-      jsPsych.endExperiment('Gracias por tu tiempo. Puedes cerrar esta pestaña.');
-    } else {
-      data.response = "Accepted";
+      jsPsych.endExperiment(Consent_001_end);
     }
   },
     data: {
     trialid: 'Consent_001',
     procedure: 'Consent'
    }
+
+
 };
 Consent.push(question01);
 
@@ -41,4 +66,3 @@ Consent.push(question01);
 Consent.unshift(instruction_screen_experiment);
 Consent.push.apply(questions, Consent);
 call_function("Consent");
-
