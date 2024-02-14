13004846e06eb8a0c5046c26b256c1668a56ca7c
G <gorkang@gmail.com>
2024-02-14 13:25:46 +0000

    add warning if disable_button_interaction is true but debug_mode is not

--- canonical_clean_6/controllers/js/helper_functions.js
+++ canonical_clean_6/controllers/js/helper_functions.js
@@ -518,6 +518,7 @@ function image_zoom() {
           // While testing, do not disable. Monkeys are not ready yet
           disable_button = true
           if (debug_mode == true) disable_button = disable_button_interaction
+          if (debug_mode == false && disable_button_interaction == true) console.warn('disable_button_interaction only works when debug_mode == true')
 
           document.querySelector("[id$=next]").disabled = disable_button;
           document.querySelector("[id$=next]").title = text_error_zoom;
